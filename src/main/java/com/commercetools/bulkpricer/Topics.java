7dc7259ba807cf47b51837a6b589ec83c24be6b6
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2018-03-30 21:41:51 +0200

    fix event topic names

--- src/main/java/com/commercetools/bulkpricer/Topics.java
+++ src/main/java/com/commercetools/bulkpricer/Topics.java
@@ -4,7 +4,7 @@ Topic keys on the vert.x event bus
  */
 public class Topics {
   public static String loadrequests = "bulkpricer.loadrequests";
-  public static String loadresults = "bulkpricer.loadrequests";
-  public static String deleteresults = "bulkpricer.loadrequests";
+  public static String loadresults = "bulkpricer.loadresults";
+  public static String deleteresults = "bulkpricer.deleteresults";
 
 }
